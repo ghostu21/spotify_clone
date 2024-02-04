5c8814aa3e256ed21c9f52ce2284e59f474e66fc
ghostu21 <mayanklahoti345@gmail.com>
2024-02-04 16:58:03 +0530

    Initial commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,13 @@
+import { Error,Loader,SongCard } from "../components";
+import { genres } from '../assets/constants'
+const Discover = () => {
+return(
+    console.log(genres);
+<div> 
+
+</div>
+
+);
+};
+
+export default Discover;
